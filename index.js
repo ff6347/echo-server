1d2469f0fe69efe698e9ab8817e5f0eb18e6bc6f
ff6347 <hash@ff6347.email>
2022-07-25 10:43:21 +0200

    style: Use Tabs

--- index.js
+++ index.js
@@ -2,25 +2,25 @@ import fastify from "fastify";
 
 // fastify echo server for testing
 const server = fastify({
-  logger: true,
+	logger: true,
 });
 server.get("/", (request, reply) => {
-  reply.send({ hello: "world" });
+	reply.send({ hello: "world" });
 });
 server.get("/*", (request, reply) => {
-  reply.send({ url: request.url });
+	reply.send({ url: request.url });
 });
 
 server.post("/", (request, reply) => {
-  reply.send(request.body);
+	reply.send(request.body);
 });
 server.post("/*", (request, reply) => {
-  reply.send({ body: request.body, url: request.url });
+	reply.send({ body: request.body, url: request.url });
 });
-server.listen(8080, (err, address) => {
-  if (err) {
-    server.log.error(err);
-    process.exit(1);
-  }
-  server.log.info(`server listening on ${address}`);
+server.listen(8000, (err, address) => {
+	if (err) {
+		server.log.error(err);
+		process.exit(1);
+	}
+	server.log.info(`server listening on ${address}`);
 });
