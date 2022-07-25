648be04d4ae0f1636177168138948436b6f640c3
ff6347 <hash@ff6347.email>
2022-07-25 10:36:28 +0200

    init

---
+++ index.js
@@ -0,0 +1,26 @@
+import fastify from "fastify";
+
+// fastify echo server for testing
+const server = fastify({
+  logger: true,
+});
+server.get("/", (request, reply) => {
+  reply.send({ hello: "world" });
+});
+server.get("/*", (request, reply) => {
+  reply.send({ url: request.url });
+});
+
+server.post("/", (request, reply) => {
+  reply.send(request.body);
+});
+server.post("/*", (request, reply) => {
+  reply.send({ body: request.body, url: request.url });
+});
+server.listen(8080, (err, address) => {
+  if (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+  server.log.info(`server listening on ${address}`);
+});
